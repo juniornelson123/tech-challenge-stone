f692aae049ab9f238fca265041ad98e06d5c8653
junior nelson <junior123nelson@gmail.com>
2019-12-04 22:52:41 -0300

    feat: View and controller for transfer api

--- apps/financial_system_web/lib/financial_system_web/controllers/api/transfer_controller.ex
+++ apps/financial_system_web/lib/financial_system_web/controllers/api/transfer_controller.ex
@@ -5,9 +5,13 @@ defmodule FinancialSystemWeb.Api.TransferController do
   alias FinancialSystem.Repo
 
   def create(conn, %{"account_id" => account_id, "transfer" => transfer_params}) do
+    transfer_params = transfer_params |> Enum.map(fn item -> %{account_received_id: item["account_id"], value: item["value"]} end)
 
     case FinancialSystem.transfer(account_id, transfer_params) do
       {:ok, transfer} ->
+        conn
+        |> render("transfer.json", transfer: transfer)
+      {:error, transfer} ->
         conn
         |> render("transfer.json", transfer: transfer)
       {:error, changeset} ->
--- apps/financial_system_web/lib/financial_system_web/views/api/account_view.ex
+++ apps/financial_system_web/lib/financial_system_web/views/api/account_view.ex
@@ -2,7 +2,7 @@ defmodule FinancialSystemWeb.Api.AccountView do
   use FinancialSystemWeb, :view
 
   def render("index.json", %{accounts: accounts}) do
-    %{data: render_many(accounts, FinancialSystemWeb.Api.AccountView, "account.json")}
+    render_many(accounts, FinancialSystemWeb.Api.AccountView, "account.json")
   end
 
   def render("account.json", %{account: account}) do
