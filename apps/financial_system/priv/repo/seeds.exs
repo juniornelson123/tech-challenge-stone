b43339c7975f3b4c0654d7f8c3d36cb687aeeac6
junior nelson <junior123nelson@gmail.com>
2019-12-04 22:50:01 -0300

    feat: Create seed for accounts

---
+++ apps/financial_system/priv/repo/seeds.exs
@@ -0,0 +1,3 @@
+FinancialSystem.Transaction.create_account(%{name: "Nelson", email: "nelson@email.com", balance: "1_000_000"})
+FinancialSystem.Transaction.create_account(%{name: "Pablo", email: "pablo@email.com", balance: "1_000_000"})
+FinancialSystem.Transaction.create_account(%{name: "Elias", email: "elias@email.com", balance: "1_000_000"})
